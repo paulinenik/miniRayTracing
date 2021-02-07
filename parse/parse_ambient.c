e52be26ae3088aea6bfe14e3bb2e222c2561b3f1
Raguel Gordon <polinaarcher@gmail.com>
2021-02-07 22:15:17 +0300

    initial parse for resolution and ambient

--- parse/parse_ambient.c
+++ parse/parse_ambient.c
@@ -6,7 +6,7 @@
 /*   By: rgordon <rgordon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 18:17:20 by rgordon           #+#    #+#             */
-/*   Updated: 2021/02/06 18:22:33 by rgordon          ###   ########.fr       */
+/*   Updated: 2021/02/07 22:12:52 by rgordon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,11 @@ void	parse_ambient(char *line, t_scene *scene)
 	if (ft_strncmp(arr[0], "A", ft_strlen(arr[0])))
 		printf("map error element doesn't exist\n");
 	scene->a.brightness = ft_atof(arr[1]);
-	scene->a.rgb = ft_atorgb(arr[2]);
+	if (scene->a.brightness < 0)
+		printf("map error brightnes");
+	scene->a.color = atorgb(arr[2]);
 	// if (arr[3])
 	// 	printf("map error extra chars\n");
-	printf("width is %d height is %d", scene->a.brightness, scene->a.rgb);
-}
-
-int		main(void)
-{
-	t_scene *scene;
-
-	scene = (t_scene *)malloc(sizeof (t_scene));
-	parse_resolution("Rs 19f20 1080 kd", scene);
-	return (0);
+	printf("brigthness is %f\n", scene->a.brightness);
+	printf("red %d green %d blue %d", scene->a.color.red, scene->a.color.green, scene->a.color.blue);
 }
--- parse/parse_resolution.c
+++ parse/parse_resolution.c
@@ -6,43 +6,11 @@
 /*   By: rgordon <rgordon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 17:13:41 by rgordon           #+#    #+#             */
-/*   Updated: 2021/02/06 18:16:17 by rgordon          ###   ########.fr       */
+/*   Updated: 2021/02/07 22:02:44 by rgordon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
-
-static int		ft_isspace(int c)
-{
-	if (c == 32)
-		return (1);
-	return (0);
-}
-
-int			rt_atoi(const char *str)
-{
-	int sign;
-	int num;
-
-	sign = 1;
-	num = 0;
-	while (ft_isspace(*str))
-		str++;
-	// if (!ft_isdigit(*str))
-	// 	// map error
-	while (ft_isdigit(*str))
-	{
-		num = num * 10 + *str - 48;
-		str++;
-		if (!ft_isdigit(*str))
-		{
-			if (!ft_isspace(*str) && *str)
-				printf("map error\n");
-			return (num * sign);
-		}
-	}
-	return (0);
-}
 
 void	parse_resolution(char *line, t_scene *scene)
 {
@@ -55,6 +23,8 @@ void	parse_resolution(char *line, t_scene *scene)
 		printf("map error element doesn't exist\n");
 	scene->r.width = rt_atoi(arr[1]);
 	scene->r.height = rt_atoi(arr[2]);
+	if (scene->r.width <= 0 || scene->r.height <= 0)
+	//"map error resolution corruped"
 	// if (arr[3])
 	// 	printf("map error extra chars\n");
 	printf("width is %d height is %d", scene->r.width, scene->r.height);
